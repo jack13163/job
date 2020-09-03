1f4471c33023d228c2c8723bcf66e4bdc10a1727
jack13163 <18163132129@163.com>
2020-09-03 11:21:44 +0800

    update

--- src/jack/sort/MergeSort.java
+++ src/jack/sort/MergeSort.java
@@ -68,20 +68,4 @@ public class MergeSort {
             arr[l + j] = tmp[j];
         }
     }
-
-    /**
-     * 交换数组中两个元素
-     *
-     * @param arr
-     * @param a
-     * @param b
-     */
-    public static void swap(int[] arr, int a, int b) {
-        if (a == b) {
-            return;
-        }
-        arr[a] ^= arr[b];
-        arr[b] ^= arr[a];
-        arr[a] ^= arr[b];
-    }
 }
